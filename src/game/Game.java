74965eb204cb70e70c63d9273287862ce0fac384
TravisLeblanc1 <travis.leblanc3002@gmail.com>
2025-02-14 22:06:08 +0100

    movement 1

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,17 +1,31 @@
 package game;
 
+import javax.swing.Timer;
+
 import util.Direction;
 import util.Vector;
 
 public class Game {
+    public static final int TIC = 1000/60; // nb ms for a tic on the game (each tic is an update of the characters)
+    private Timer updateTimer = new Timer(TIC, e -> updateCharacters());
 	public Map map = new Map();
 	public Player player = new Player();
 	
+	public void start() {
+		updateTimer.start();
+		player.resetSpeed();
+	}
+	
 	public void majCharacter(Direction dir) {
+		player.changeDirection(dir);
+	}
+	
+	private void updateCharacters() {
 		var old = player.getPos();
-		player.move(dir);
+		player.move();
 		if (map.isNotValid(player.getHitbox())) {
 			player.teleport(old);
+			player.resetSpeed();
 		}
 	}
 }
